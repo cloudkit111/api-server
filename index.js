d804755e1e5b11c0af08a6fefb78e0b7c259edb6
codebyviral <viralvaghela2641@gmail.com>
2026-03-28 10:48:57 +0530

    fix cors

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "https://cloud-kit.app" },
 });
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://cloud-kit.app",
   credentials: true,
   methods: "GET, POST, DELETE, PATCH, HEAD, PUT, OPTIONS",
   allowedHeaders: [
